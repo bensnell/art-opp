1b41ed46ffca5838876db394067e3fca619768fb
Ben Snell <BenSnell@Ben-Snells-MacBook-Pro.local>
2019-01-28 18:11:28 -0500

    Changes

--- get-open-calls.py
+++ get-open-calls.py
@@ -1,11 +1,18 @@
 import os
 import sys
 import requests
+from tinydb import TinyDB, Query
+
+# Get the current location of this script
+
 
 # ========== PARAMS ============
 
 # Url to get the listings
 postUrl = "https://www.nyfa.org/Opportunities/Search"
+
+# Create a database if one doesn't exist
+db = TinyDB("db.json")
 
 # =========== CODE =============
 
@@ -16,14 +23,26 @@ try:
 except:
 	print("Could not retrieve the url " + postUrl)
 
-# Parse the information
-newOC = []
+# Parse the information for opportunities
+recentOpp = []
 if resp != None:
-	data = resp.json()["Regular_Listings"]
-	# Find all open calls
-	for opp in data:
-		if opp["OppType"] == "Call for Entry/Open Call":
-			newOC.append(opp)
+
+	try:
+		data = resp.json()["Regular_Listings"]
+		# Find all open calls
+		for opp in data:
+			if opp["OppType"] == "Call for Entry/Open Call":
+				recentOpp.append(opp)
+	except:
+		print("Something went wrong!")
+
+# Find all of the new oppportunities
+newOpp = []
+for opp in recentOpp:
+	if 
+
+
+
 
 # Add all new open calls to an RSS feed
 
@@ -32,4 +51,16 @@ if resp != None:
 # Save the new open calls to a database
 
 
+
+
+def main():
+
+	while (True):
+
+
+
+
+
+if __name__ == "__main__":
+	main()
 
