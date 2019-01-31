8b54cc824fd6a8f010f58d45ad997a9f1a8b3729
bensnell <bensnellstudio@gmail.com>
2019-01-30 22:02:17 -0500

    Changes

--- get-open-calls.py
+++ get-open-calls.py
@@ -32,7 +32,7 @@ def wrapLinks(string):
 # ========== USERPARAMS ============
 
 # Title to save this document
-saveTitle = "open-calls-4"
+saveTitle = "open-calls-5"
 
 # Url to get the listings
 listingsURL = "https://www.nyfa.org/Opportunities/Search"
