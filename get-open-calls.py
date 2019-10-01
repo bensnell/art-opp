779a0a6844ab7f5e5b7ea1fc23b572112481a251
Ubuntu <ubuntu@ip-172-31-88-11.ec2.internal>
2019-10-01 03:21:08 +0000

    Update list to 7

--- get-open-calls.py
+++ get-open-calls.py
@@ -32,7 +32,7 @@ def wrapLinks(string):
 # ========== USERPARAMS ============
 
 # Title to save this document
-saveTitle = "open-calls-6"
+saveTitle = "open-calls-7"
 
 # Url to get the listings
 listingsURL = "https://www.nyfa.org/Opportunities/Search"
